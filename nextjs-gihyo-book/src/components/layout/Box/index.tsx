de1b1c3c8abca09cb92bb02d0e243ecfebea9e88
Tatsukawa <takun.cloud961@gmail.com>
2023-05-10 18:21:26 +0900

    propsの修正

--- nextjs-gihyo-book/src/components/layout/Box/index.tsx
+++ nextjs-gihyo-book/src/components/layout/Box/index.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import type { Responsive } from "types/styles";
 import { toPropValue, Color, Space } from "utils/styles";
 
-// Boxがとりうるプロパティを列挙
 export type BoxProps = {
   color?: Responsive<Color>;
   backgroundColor?: Responsive<Color>;
@@ -28,29 +27,28 @@ export type BoxProps = {
 
 /**
  * Boxコンポーネント
- * レイアウトの調整に利用
- * ${(props) => toPropValue("color", props.color, props.theme)}
+ * レイアウトの調整に利用する
  */
 const Box = styled.div<BoxProps>`
   ${(props) => toPropValue("color", props.color, props.theme)}
-  ${(props) => toPropValue("backgroundColor", props.backgroundColor, props.theme)}
+  ${(props) => toPropValue("background-color", props.backgroundColor, props.theme)}
   ${(props) => toPropValue("width", props.width, props.theme)}
   ${(props) => toPropValue("height", props.height, props.theme)}
-  ${(props) => toPropValue("minWidth", props.minWidth, props.theme)}
-  ${(props) => toPropValue("minHeight", props.minHeight, props.theme)}
+  ${(props) => toPropValue("min-width", props.minWidth, props.theme)}
+  ${(props) => toPropValue("min-height", props.minHeight, props.theme)}
   ${(props) => toPropValue("display", props.display, props.theme)}
   ${(props) => toPropValue("border", props.border, props.theme)}
   ${(props) => toPropValue("overflow", props.overflow, props.theme)}
   ${(props) => toPropValue("margin", props.margin, props.theme)}
   ${(props) => toPropValue("margin-top", props.marginTop, props.theme)}
-  ${(props) => toPropValue("margin-right", props.marginRight, props.theme)}
-  ${(props) => toPropValue("margin-bottom", props.marginBottom, props.theme)}
   ${(props) => toPropValue("margin-left", props.marginLeft, props.theme)}
+  ${(props) => toPropValue("margin-bottom", props.marginBottom, props.theme)}
+  ${(props) => toPropValue("margin-right", props.marginRight, props.theme)}
   ${(props) => toPropValue("padding", props.padding, props.theme)}
   ${(props) => toPropValue("padding-top", props.paddingTop, props.theme)}
-  ${(props) => toPropValue("padding-right", props.paddingRight, props.theme)}
-  ${(props) => toPropValue("padding-bottom", props.paddingBottom, props.theme)}
   ${(props) => toPropValue("padding-left", props.paddingLeft, props.theme)}
+  ${(props) => toPropValue("padding-bottom", props.paddingBottom, props.theme)}
+  ${(props) => toPropValue("padding-right", props.paddingRight, props.theme)}
 `;
 
 export default Box;
