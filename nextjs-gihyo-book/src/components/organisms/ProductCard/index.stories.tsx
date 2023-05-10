c616f70e7afa2c558b92624f1fd0bdc89ffe5a34
Tatsukawa <takun.cloud961@gmail.com>
2023-05-10 18:23:38 +0900

    organismsの追加

---
+++ nextjs-gihyo-book/src/components/organisms/ProductCard/index.stories.tsx
@@ -0,0 +1,81 @@
+import { Meta, StoryFn } from '@storybook/react';
+import ProductCard from './index';
+
+export default {
+  title: 'Organisms/ProductCard',
+  argTypes: {
+    title: {
+      control: { type: 'text' },
+      description: '商品名',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    price: {
+      control: { type: 'number' },
+      description: '商品価格',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    imageUrl: {
+      control: { type: 'text' },
+      description: '商品画像URL',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    blurDataUrl: {
+      control: { type: 'text' },
+      description: '商品のぼかし画像のデータURIスキーム',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    variant: {
+      options: ['listing', 'small', 'detail'],
+      control: { type: 'radio' },
+      defaultValue: 'listing',
+      description: 'バリアント（表示スタイル）',
+      table: {
+        type: { summary: 'listing | small | detail' },
+        defaultValue: { summary: 'primary' },
+      },
+    },
+  },
+} as Meta<typeof ProductCard>;
+
+const Template: StoryFn<typeof ProductCard> = (args) => <ProductCard {...args} />;
+
+export const Listing = {
+  render: Template,
+
+  args: {
+    variant: 'listing',
+    title: 'ナイスシューズ',
+    imageUrl: '/images/sample/1.jpg',
+    price: 2000,
+  },
+};
+
+export const Small = {
+  render: Template,
+
+  args: {
+    variant: 'small',
+    title: 'ナイスシューズ',
+    imageUrl: '/images/sample/1.jpg',
+    price: 2000,
+  },
+};
+
+export const Detail = {
+  render: Template,
+
+  args: {
+    variant: 'detail',
+    title: 'ナイスシューズ',
+    imageUrl: '/images/sample/1.jpg',
+    price: 2000,
+  },
+};
---
+++ nextjs-gihyo-book/src/components/organisms/SigninForm/index.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryFn } from '@storybook/react';
+import SigninForm from './index';
+
+export default {
+  title: 'Organisms/SigninForm',
+  argTypes: {
+    onSignin: {
+      description: 'サインインボタンを押した時のイベントハンドラ',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+  },
+} as Meta<typeof SigninForm>;
+
+const Template: StoryFn<typeof SigninForm> = (args) => <SigninForm {...args} />;
+export const Form = {
+  render: Template,
+};
